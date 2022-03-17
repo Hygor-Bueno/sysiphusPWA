29238f62bf823dcfa069ae27a5fd152588d98859
Hygor Bueno <hygor_knd.x1@hotmail.com>
2022-03-17 12:30:22 -0300

    Ajuste PWA

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
 
-const el = document.createElement('pwa-update');
-document.body.appendChild(el);
 
 
 import { PageApp } from "./Components/Page/page.js";

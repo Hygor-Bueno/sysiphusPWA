ea0dc44c8f49c855f735a187416fbc024fb2ca72
Hygor Bueno <hygor_knd.x1@hotmail.com>
2022-03-18 15:24:03 -0300

    created tags 'header', 'aside', 'section','footer' of tamplate and buttons. Template configured style.

--- Components/Checklist/checklistGenerator.js
+++ Components/Checklist/checklistGenerator.js
@@ -2,7 +2,29 @@ export class checklistGenerator{
     template(){
         return `
         <div id="divCreateChecklist">
+            <header>
+                <p><b>+</b> Criar novo Checklist</p>
+                
+                <div id="dayQuantits">
+                    <button type="button" class="dayQuantitsButton" value="1" title="Lista de tarefas para o dia"> D </button>
+                    <button type="button" class="dayQuantitsButton" value="7" title="Lista de tarefas para a semana" disabled> S </button>
+                    <button type="button" class="dayQuantitsButton" value="31" title="Lista de tarefas para o mês" disabled> M </button>
+                </div> 
+            </header>            
             
+            <aside>
+                
+            </aside>
+                
+            <section>
+                
+            </section>
+           
+            <footer>
+                <button type="button" class="buttonDefualt"> Limpar </button>
+                <button type="button" class="buttonDefualt"> Cancelar </button>
+                <button type="button" class="buttonDefualt"> Enviar </button> 
+            </footer>
         </div>
         `
     }
